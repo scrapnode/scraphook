816ab778b5d41aa221f83fdab1f6b884d2526a7d
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2023-01-02 08:57:39 +0700

    chore: add go fmt to pre-commit hook

---
+++ scripts/fmt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+## this will retrieve all of the .go files that have been
+## changed since the last commit
+STAGED_GO_FILES=$(git diff --cached --name-only -- '*.go')
+
+## we can check to see if this is empty
+if [[ $STAGED_GO_FILES != "" ]]; then
+    for file in $STAGED_GO_FILES; do
+        go fmt "$file"
+        git add "formatted $file"
+    done
+fi
