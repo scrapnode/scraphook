436c181a302e400588361fca635a31d2209cefcb
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2023-01-02 08:57:46 +0700

    chore: add go fmt to pre-commit hook

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -10,6 +10,6 @@ STAGED_GO_FILES=$(git diff --cached --name-only -- '*.go')
 if [[ $STAGED_GO_FILES != "" ]]; then
     for file in $STAGED_GO_FILES; do
         go fmt "$file"
-        git add "formatted $file"
+        git add "$file"
     done
 fi
