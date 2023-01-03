232c75470cf01e26010a0168f4e63440735aa86f
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2023-01-03 11:44:57 +0700

    chore: bump git@github.com:scrapnode/scrapcore.git to 9cb43c1d31f38f993f1b7865f01b3f0d35026837

--- db/migrations/000001_webhook.up.sql
+++ db/migrations/000001_webhook.up.sql
@@ -4,11 +4,9 @@ CREATE TABLE IF NOT EXISTS workspaces (
     name VARCHAR(256) NOT NULL,
     created_at BIGINT DEFAULT 0,
     updated_at BIGINT DEFAULT 0,
-    deleted_at BIGINT DEFAULT 0,
     CONSTRAINT scraphook_workspaces_pk PRIMARY KEY (id)
 );
 CREATE INDEX scraphook_workspaces_idx_user_id ON workspaces (user_id);
-CREATE INDEX scraphook_workspaces_idx_deleted_at ON workspaces (deleted_at);
 
 CREATE TABLE IF NOT EXISTS webhooks (
     workspace_id VARCHAR(64) NOT NULL,
@@ -16,10 +14,16 @@ CREATE TABLE IF NOT EXISTS webhooks (
     name VARCHAR(256) NOT NULL,
     created_at BIGINT DEFAULT 0,
     updated_at BIGINT DEFAULT 0,
-    deleted_at BIGINT DEFAULT 0,
     CONSTRAINT scraphook_webhooks_pk PRIMARY KEY (id)
 );
-CREATE INDEX scraphook_webhooks_idx_user_id ON workspaces (user_id);
-CREATE INDEX scraphook_webhooks_idx_deleted_at ON workspaces (deleted_at);
+CREATE INDEX scraphook_webhooks_idx_workspace_id ON webhooks (workspace_id);
 
-
+CREATE TABLE IF NOT EXISTS webhook_tokens (
+    webhook_id VARCHAR(64) NOT NULL,
+    id VARCHAR(64) NOT NULL,
+    token VARCHAR(256) NOT NULL,
+    created_at BIGINT DEFAULT 0,
+    updated_at BIGINT DEFAULT 0,
+    CONSTRAINT scraphook_webhook_tokens_pk PRIMARY KEY (id)
+);
+CREATE INDEX scraphook_webhooks_idx_webhook_id ON webhook_tokens (webhook_id);
